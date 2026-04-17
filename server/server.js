bd4bcb505ca029cdf816052b2fc26b35b1cecc9b
Ayush Mishra <ayushmg2007@gmail.com>
2026-04-18 04:30:31 +0530

    Initialize monorepo with MERN foundation and User schema

---
+++ server/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express=require('express');
+const mongoose=require('mongoose');
+const cors=require('cors');
+const app=express();
+app.use(cors());
+app.use(express.json());
+const dburi=process.env.MONGOURI;
+mongoose.connect(dburi);
+const portnum=process.env.PORT||5000;
+app.listen(portnum);
